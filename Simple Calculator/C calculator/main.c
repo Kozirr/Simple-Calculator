a4206d34d2456fc7d882dec6ac3cdd4da69daff8
Nurəddin Qurbanov <166313158+Kozirr@users.noreply.github.com>
2025-08-25 00:05:00 +0400

    first calculator
    simple calculator in c language

---
+++ Simple Calculator/C calculator/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <conio.h>
+
+int arithmetics(){
+
+    float num1, num2;
+    char op;
+
+    printf("Calculator: ");
+   
+    if (scanf("%f %c %f", &num1, &op, &num2) != 3) {
+        printf("Invalid input\n");
+        printf("Press ESC to exit, any other key to continue...\n");
+        char key = getch();
+        if (key == 27) {  
+        return 0;
+        }
+    }
+
+    while(getchar() != '\n'); 
+
+    if(op == '+'){
+        printf("%f\n", num1 + num2);
+        printf("Press ESC to exit, any other key to continue...\n");
+        char key = getch();
+        if (key == 27) {  
+    return 0;    
+        }
+    }else if(op == '-'){
+        printf("%f\n", num1 - num2);
+        printf("Press ESC to exit, any other key to continue...\n");
+        char key = getch();
+        if (key == 27) {
+            return 0;
+        }
+
+    }else if(op == '*'){
+        printf("%f\n", num1 * num2);
+        printf("Press ESC to exit, any other key to continue...\n");
+        char key = getch();
+        if (key == 27) {
+            return 0;
+        }
+    }else if(op == '/'){
+        if(num2 != 0){
+            printf("%f\n", num1 / num2);
+            printf("Press ESC to exit, any other key to continue...\n");
+            char key = getch();
+            if (key == 27) {
+            return 0;
+        }
+        }else{
+            printf("Cannot divide by zero\n");
+            printf("Press ESC to exit, any other key to continue...\n");
+            char key = getch();
+            if (key == 27) {
+            return 0;
+        }
+        }
+    }else{
+        printf("Invalid operator\n");
+        printf("Press ESC to exit, any other key to continue...\n");
+        char key = getch();
+        if (key == 27) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main() {
+
+    while(arithmetics());
+
+   return 0;
+}
+
